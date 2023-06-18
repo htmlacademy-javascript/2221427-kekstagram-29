1ae0d6243f6e4009fa661d0a6c717fb8e662ccd7
лев елистратов <msijkeee@gmail.com>
2023-06-19 00:34:59 +0700

    добавил  модули

---
+++ js/consts.js
@@ -0,0 +1,13 @@
+const TOTAL_PHOTO_AMOUNT = 25;
+
+const LIKES_AMOUNT = {
+  min: 15,
+  max: 200,
+};
+
+const COMMENTS_AMOUNT = {
+  min: 0,
+  max: 30,
+};
+
+export {TOTAL_PHOTO_AMOUNT, LIKES_AMOUNT, COMMENTS_AMOUNT};
