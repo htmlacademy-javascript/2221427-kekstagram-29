1ef9dee400e7e705e180c2887bfc2045039b7567
лев елистратов <msijkeee@gmail.com>
2023-07-12 22:48:13 +0700

    правки к 7 заданию

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
 import {createPhotos} from './mocks/mock-functions.js';
-// eslint-disable-next-line no-unused-vars
-import {drawOtherPhotos} from './thumbnail-pictures.js';
+import {renderThumbnailsPictures} from './thumbnail-pictures.js';
+renderThumbnailsPictures(createPhotos);
+
--- js/thumbnail-pictures.js
+++ js/thumbnail-pictures.js
@@ -1,23 +1,20 @@
-import {createPhotos} from './mocks/mock-functions.js';
-
 const pictureContainer = document.querySelector('.pictures');
 pictureContainer.classList.remove('visually-hidden');
 const picturesTemlpate = document.querySelector('#picture').content.querySelector('.picture');
-const drawOtherPhotos = createPhotos();
 
-const pictureContainerFragment = document.createDocumentFragment();
+const renderThumbnailsPictures = (drawOtherPhotos) => {
+  const pictureContainerFragment = document.createDocumentFragment();
+  drawOtherPhotos().forEach((photo)=> {
+    const userPhotoElement = picturesTemlpate.cloneNode(true);
+    userPhotoElement.querySelector('.picture__img').src = photo.url;
+    userPhotoElement.querySelector('.picture__img').alt = photo.description;
+    userPhotoElement.querySelector('.picture__likes').textContent = photo.likes;
+    userPhotoElement.querySelector('.picture__comments').textContent = photo.comments.length;
 
-drawOtherPhotos.forEach((photo)=> {
-  const userPhotoElement = picturesTemlpate.cloneNode(true);
-  userPhotoElement.querySelector('.picture__img').src = photo.url;
-  userPhotoElement.querySelector('.picture__img').alt = photo.description;
-  userPhotoElement.querySelector('.picture__likes').textContent = photo.likes;
-  userPhotoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    pictureContainerFragment.append(userPhotoElement);
 
-  pictureContainerFragment.appendChild(userPhotoElement);
+  });
 
-});
-
-pictureContainer.append(pictureContainerFragment);
-
-export {drawOtherPhotos};
+  pictureContainer.append(pictureContainerFragment);
+};
+export {renderThumbnailsPictures};
