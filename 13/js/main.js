653b72cad1aee67285778e28a7881bb3bf11980d
Keks <keks@htmlacademy.ru>
2023-07-25 22:06:23 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,6 @@
+import {createPhotos} from './mocks/mock-functions.js';
+import {renderThumbnailsPictures} from './thumbnail-pictures.js';
+renderThumbnailsPictures(createPhotos());
+import {submitForm} from './form/main-form.js';
+submitForm();
+
