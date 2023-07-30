6b6d9a7818943fb3cfa5a6ffcdd5f3db220d07b7
лев елистратов <msijkeee@gmail.com>
2023-07-31 00:04:48 +0700

    fixes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import {renderThumbnailsPictures} from './thumbnail-pictures.js';
-import {addValidationAndListeners,setUserFormSubmit} from './form/main-form.js';
+import {addValidationAndListeners} from './form/main-form.js';
 import {getData} from './api.js';
 
 getData((photos) => {
   renderThumbnailsPictures(photos);
 });
-setUserFormSubmit(addValidationAndListeners);
+addValidationAndListeners();
